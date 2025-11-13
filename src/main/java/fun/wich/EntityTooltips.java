67adff31e7d06bcd2e58f03992625e487935cf57
OfficePigeon <ava@wich.fun>
2025-11-12 20:15:06 -0500

    Bugfixes

--- src/main/java/fun/wich/EntityTooltips.java
+++ src/main/java/fun/wich/EntityTooltips.java
@@ -1,14 +1,8 @@
 package fun.wich;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EntityTooltips implements ModInitializer {
-	public static final String MOD_ID = "wich";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
-	public void onInitialize() {
-		LOGGER.info("Entity Tooltips - ACTIVE");
-	}
+	public void onInitialize() { }
 }
